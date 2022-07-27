95c9e94e40dada17f879fd1286d57077a639a3ca
kravetsd <kdykrg@gmail.com>
2022-07-27 20:16:14 +0000

    jolly good. two function excercise

---
+++ ch2/excercises/jollygood.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void)
+{
+    void jolly(void);
+    void deny(void);
+
+
+    jolly();
+    jolly();
+    jolly();
+    deny();
+
+    return 0;
+}
+
+void deny(void)
+{
+    printf("Which nobody can deny!\n");
+}
+
+void jolly(void)
+{
+    printf("For he's a jolly good fellow!\n");
+}
