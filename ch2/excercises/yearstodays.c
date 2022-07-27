3a095d8dd19888473497c2f6c177bdd43bea7e9c
kravetsd <kdykrg@gmail.com>
2022-07-27 16:06:51 +0000

    programm that counts days to years

---
+++ ch2/excercises/yearstodays.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    int years, days;
+    years = 30;
+
+    printf("My age is %d years old\n", years);
+    printf("My age is %d days\n", years*365);
+
+    return 0;
+}
