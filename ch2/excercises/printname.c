aeaad35b39211973ec0b7aeff1390f32e64e1f94
kravetsd <kdykrg@gmail.com>
2022-07-27 15:59:06 +0000

    first excercise done

---
+++ ch2/excercises/printname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    printf("Dmitriy Kravets\n");
+    printf("Dmitriy\nKravets\n");
+    printf("Dmitriy ");
+    printf("Kravets\n");
+
+    return 0;
+
+
+
+}
