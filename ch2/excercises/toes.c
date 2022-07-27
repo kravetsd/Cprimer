3c3e030ec257794637f4b4ca17d46f12e8dbe18a
kravetsd <kdykrg@gmail.com>
2022-07-27 20:35:12 +0000

    toes exercise added

---
+++ ch2/excercises/toes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    int toes;
+
+    toes = 10;
+    printf("toes = %d\ntwice toes = %d\ntoes squared = %d\n",toes, 2*toes,toes*toes);
+    
+    return 0;
+}
