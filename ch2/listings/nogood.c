890e48277067ad61fccce65625753b6347ec9537
kravetsd <kdykrg@gmail.com>
2022-07-27 15:07:33 +0000

    added nogood.c code

---
+++ ch2/listings/nogood.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n; int n2; int n3;
+
+    // this program has several errors 
+
+    n = 5;
+    n2 = n*n;
+    n3 = n2*n2;
+
+    printf("n = %d, n2 squared = %d, n3 cubed = %d \n",n,n2,n3);
+}
