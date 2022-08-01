3a879b6720efb7f0df8f293d11f8b1409d84dfc4
kravetsd <kdykrg@gmail.com>
2022-08-01 21:04:56 +0000

    entered integer and converted to ASCII

---
+++ ch3/excercises/2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    int sym;
+    printf("Please enter the asci code for a symbol to print\n");
+    scanf("%d",&sym);
+
+    printf("You entered %d, which is %c in ASCI.\n", sym,sym);
+
+    return 0;
+}
