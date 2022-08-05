4bcb2e04aa3c2673db09a65f5f36f1c717306286
kravetsd <kdykrg@gmail.com>
2022-08-05 12:59:33 +0000

    advanced printf conversion

--- ch4/listings/intconv.c
+++ ch4/listings/intconv.c
@@ -5,6 +5,15 @@ int main(void)
 {
     short num = PAGES;
     short mnum = -PAGES;
+
+    short tip = 120000;
+
+    printf("short is %zd bytes\n", sizeof(short));
+    printf("int is %zd bytes\n", sizeof(int));
+
+    printf("as unsigned %hu\n", tip);
+    printf("as signed %hd\n", tip);
+    printf("unsigned short is %zd bytes\n", sizeof(unsigned short));    
     printf("num as short and unsigned short: %hd %hu\n", num, num);
     printf("-num as short and unsigned short: %hd %hu\n", mnum, mnum);
     printf("num as int and char: %d %c\n", num, num);
