2d0774c94ef460989c305f59d7fccdb0f6c4335a
kravetsd <kdykrg@gmail.com>
2022-08-10 21:34:45 +0000

    LPG to KMPL

--- ch4/excercises/8_miles_gallons.c
+++ ch4/excercises/8_miles_gallons.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
-
+#define KMPM 1.609
+#define LPG 3.785
 int main(void)
 {
     float miles, gallons;
-    printf("Please enter a number of miles you travaled: \n");
+    printf("Please enter a number of miles you traveled: \n");
     scanf("%f", &miles);
 
     printf("Please enter a number of gallons you used: \n");
     scanf("%f", &gallons);
 
+
     printf("miles-per-gallon: %.3f\n", miles/gallons);
+
+    printf("liters-per-100-km: %.1f\n", (100*LPG)/((miles/gallons)*KMPM));
     
 
     return 0;
