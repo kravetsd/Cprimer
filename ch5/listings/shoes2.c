fb8025ec9b5e70d92b66fe1c620535d07484a8ab
kravetsd <kdykrg@gmail.com>
2022-08-18 21:48:09 +0000

    Added a while loop for tests loops

---
+++ ch5/listings/shoes2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define ADJUST 7.31
+
+int main(void) {
+    const double SCALE = 0.333;
+
+    double shoe, foot;
+
+    shoe = 3.0;
+
+    while (shoe < 18.0)
+    {
+        
+        foot = SCALE*shoe + ADJUST;
+        printf("Shoe size (men's)   foot length\n");
+        printf("%10.1f %15.2f inches\n",shoe, foot);
+
+        shoe = shoe + 1.0;
+
+    }
+
+    printf("If shoe feets your needs - wear it!\n");
+    
+
+
+    return 0;
+}
