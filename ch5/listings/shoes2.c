b9fa0e076d87e3f5029fffe35fb17a2120464517
kravetsd <kdykrg@gmail.com>
2022-08-18 21:48:56 +0000

    Fixed shoes2 output

--- ch5/listings/shoes2.c
+++ ch5/listings/shoes2.c
@@ -7,12 +7,12 @@ int main(void) {
     double shoe, foot;
 
     shoe = 3.0;
+    printf("Shoe size (men's)   foot length\n");
 
     while (shoe < 18.0)
     {
         
         foot = SCALE*shoe + ADJUST;
-        printf("Shoe size (men's)   foot length\n");
         printf("%10.1f %15.2f inches\n",shoe, foot);
 
         shoe = shoe + 1.0;
